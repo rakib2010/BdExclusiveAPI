91afa1763d2a24577974bf8af101e9e13637c4c4
Rakib <rakib.hasan.asia@gmail.com>
2022-03-19 23:08:09 +0600

    change

---
+++ src/main/java/com/example/CartController.java
@@ -0,0 +1,116 @@
+
+package com.example;
+
+import com.example.model.Cart;
+import com.example.model.Product;
+import com.example.storage.service.FileStorageService;
+import java.util.List;
+import javax.transaction.Transactional;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class CartController {
+    @Autowired
+    SessionFactory sessionFactory;
+    
+    @Autowired
+    private FileStorageService fileStorageService;
+    
+    
+    @PostMapping(value = "/cart/save", consumes = {MediaType.APPLICATION_JSON_VALUE})
+    @Transactional
+    public ResponseEntity<?> save(@RequestBody Cart entity) {
+        try {
+            
+            Session session = sessionFactory.openSession();
+            session.save(entity);
+            session.flush();
+            session.close();
+            return ResponseEntity.ok("Data saved successfully");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.ok("Save failed");
+        }
+    }
+     
+     
+     
+     
+    @PostMapping(value = "/cart/addCart_withfile")
+    @Transactional
+    public ResponseEntity<?> addCart(@ModelAttribute Cart entity, @RequestParam("file") MultipartFile file) {
+        try {
+            String fileName = fileStorageService.storeFile(file);
+            String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/downloadFile/")
+                .path(fileName)
+                .toUriString();
+            
+            Session session = sessionFactory.openSession();
+            entity.setImageName(fileName);
+            entity.setImageUri(fileDownloadUri);
+            
+            session.save(entity);
+            session.flush();
+            session.close();
+            return ResponseEntity.ok("Data saved successfully");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.ok("Save failed");
+        }
+    }
+    
+    
+    
+    @GetMapping(value = "/cart/getAll")
+    public ResponseEntity<?> getAll() {
+        try {
+            Session session = sessionFactory.openSession();
+            List<Cart> entityList = session.createQuery("From Cart").list();
+            session.flush();
+            session.close();
+            return ResponseEntity.ok(entityList);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.ok("Data fetch failed!");
+        }
+    }
+    
+    
+    
+    @GetMapping(value = "/cart/delete/{id}", consumes = {MediaType.APPLICATION_JSON_VALUE})
+    @Transactional
+    public ResponseEntity<?> delete(@PathVariable(value = "id") long id) {
+        try {
+            Session session = sessionFactory.openSession();
+            Product entity = session.get(Product.class, id);
+            session.delete(entity);
+            session.flush();
+            session.close();
+            return ResponseEntity.ok("Delete successful");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.ok("Data fetch failed!");
+        }
+    }
+     
+     
+    
+    
+    
+}
---
+++ src/main/java/com/example/model/Cart.java
@@ -0,0 +1,84 @@
+
+package com.example.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "cart", schema = "jeewebdb")
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String productName;
+    private int quantity;
+    private double price;
+    private String remarks;
+    private String imageUri;
+    private String imageName;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    public String getImageUri() {
+        return imageUri;
+    }
+
+    public void setImageUri(String imageUri) {
+        this.imageUri = imageUri;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+    
+    
+    
+    
+    
+}
